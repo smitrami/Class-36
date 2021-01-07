0aeb78dffb89b69522d9e12d926319274939303e
Shreya Rami <57885502+smitrami@users.noreply.github.com>
2021-01-07 16:12:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var gameState = 0;
+var playerCount;
+var database;
+var form, player, game;
+
+function setup() {
+  createCanvas(400, 400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+function draw() {
+
+}
